5ec3b93369bfacc7970bb0f599a79a01c0307ac0
coder_tq <909413805@qq.com>
2021-05-16 11:16:03 +0800

    更新笔记及代码

--- codes/src/尚硅谷面试题第二季/CallableDemo.java
+++ codes/src/尚硅谷面试题第二季/CallableDemo.java
@@ -1,5 +1,26 @@
-package 尚硅谷面试题第二季;/**
+package 尚硅谷面试题第二季;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+/**
  * @author coder_tq
  * @Date 2021/5/16 9:28
- */public class CallableDemo {
+ */
+public class CallableDemo {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        FutureTask<Integer> futureTask = new FutureTask<>(new MyThread());
+        new Thread(futureTask,"AA").start();
+        futureTask.get();
+    }
+}
+
+class MyThread implements Callable<Integer>{
+
+    @Override
+    public Integer call() throws Exception {
+        System.out.println("Come in");
+        return 1024;
+    }
 }
--- codes/src/尚硅谷面试题第二季/ThreadPoolDemo.java
+++ codes/src/尚硅谷面试题第二季/ThreadPoolDemo.java
@@ -1,5 +1,23 @@
-package 尚硅谷面试题第二季;/**
+package 尚硅谷面试题第二季;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
  * @author coder_tq
  * @Date 2021/5/16 10:41
- */public class ThreadPoolDemo {
+ */
+public class ThreadPoolDemo {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        ExecutorService executorService1 = Executors.newCachedThreadPool();
+        ExecutorService executorService2 = Executors.newSingleThreadExecutor();
+        try {
+
+        }catch (Exception e){
+
+        }finally {
+            executorService.shutdown();
+        }
+    }
 }
