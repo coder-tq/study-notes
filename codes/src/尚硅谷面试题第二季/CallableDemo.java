7a0c3fccb8e8918acdadb6f1eceab0bccd8576c4
coder_tq <909413805@qq.com>
2021-05-17 13:56:36 +0800

    更新笔记和代码

--- codes/src/尚硅谷面试题第二季/CallableDemo.java
+++ codes/src/尚硅谷面试题第二季/CallableDemo.java
@@ -3,6 +3,7 @@ package 尚硅谷面试题第二季;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author coder_tq
@@ -20,7 +21,8 @@ class MyThread implements Callable<Integer>{
 
     @Override
     public Integer call() throws Exception {
-        System.out.println("Come in");
+        System.out.println(Thread.currentThread().getName()+"Come in");
+        try { TimeUnit.MILLISECONDS.sleep(1000); } catch (InterruptedException e) { e.printStackTrace(); }
         return 1024;
     }
 }
--- codes/src/尚硅谷面试题第二季/ThreadPoolDemo.java
+++ codes/src/尚硅谷面试题第二季/ThreadPoolDemo.java
@@ -1,7 +1,7 @@
 package 尚硅谷面试题第二季;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author coder_tq
@@ -9,15 +9,52 @@ import java.util.concurrent.Executors;
  */
 public class ThreadPoolDemo {
     public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(1);
-        ExecutorService executorService1 = Executors.newCachedThreadPool();
-        ExecutorService executorService2 = Executors.newSingleThreadExecutor();
+//        ExecutorService executorService = Executors.newFixedThreadPool(1);
+//        ExecutorService executorService1 = Executors.newCachedThreadPool();
+//        ExecutorService executorService2 = Executors.newSingleThreadExecutor();
+//        ExecutorService executorService3 = Executors.newWorkStealingPool();
+        ExecutorService executorService = new ThreadPoolExecutor(2,
+                5,
+                1L,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(4),
+                new MyThreadFactory(),
+                new ThreadPoolExecutor.AbortPolicy());
         try {
-
+            for (int i = 0; i < 10; i++) {
+                executorService.submit(new MyThread());
+            }
         }catch (Exception e){
-
+            e.printStackTrace();
         }finally {
             executorService.shutdown();
         }
+    }
+}
+class MyThreadFactory implements ThreadFactory{
+    private static final AtomicInteger poolNumber = new AtomicInteger(1);
+    private final ThreadGroup group;
+    private final AtomicInteger threadNumber = new AtomicInteger(1);
+    private final String namePrefix;
+
+    MyThreadFactory() {
+        SecurityManager s = System.getSecurityManager();
+        group = (s != null) ? s.getThreadGroup() :
+                Thread.currentThread().getThreadGroup();
+        namePrefix = poolNumber.getAndIncrement() + " 号线程池的第 ";
+    }
+
+    @Override
+    public Thread newThread(Runnable r) {
+        Thread t = new Thread(group, r,
+                namePrefix + threadNumber.getAndIncrement()+" 号线程。",
+                0);
+        if (t.isDaemon()) {
+            t.setDaemon(false);
+        }
+        if (t.getPriority() != Thread.NORM_PRIORITY) {
+            t.setPriority(Thread.NORM_PRIORITY);
+        }
+        return t;
     }
 }
