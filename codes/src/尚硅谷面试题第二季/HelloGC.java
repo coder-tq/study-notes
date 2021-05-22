11b90495a77f3c4f55d09a0b8d27c1bf961b0c07
coder_tq <909413805@qq.com>
2021-05-22 16:46:31 +0800

    更新笔记

---
+++ codes/src/尚硅谷面试题第二季/HelloGC.java
@@ -0,0 +1,17 @@
+package 尚硅谷面试题第二季;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author coder_tq
+ * @Date 2021/5/19 22:08
+ */
+public class HelloGC {
+    public static void main(String[] args) {
+        System.out.println("========================");
+        byte[] bytes = new byte[500*1024*1024];
+    }
+}
